b336e5bca7e7adb859e0afb7dd097baf10dc52df
sonjagnt <signtieva@gmail.com>
2025-07-08 16:56:05 +0300

    fix firebase database path

--- src/service/firebase-api.js
+++ src/service/firebase-api.js
@@ -11,7 +11,7 @@ import { database } from "../utils/firebase";
 import { getAuth } from "firebase/auth";
 
 export const fetchTeachers = async (startFrom = null, pageSize = 4) => {
-  const teachersRef = ref(database, "/");
+  const teachersRef = ref(database, "/teachers");
 
   const postsQuery = startFrom
     ? query(
@@ -41,12 +41,23 @@ export const addToFavorites = async (userId, itemId, itemDetails) => {
   if (!user) {
     throw new Error("User is not authenticated");
   }
-
   const favoritesRef = ref(database, `users/${userId}/favorites/${itemId}`);
   try {
     await set(favoritesRef, itemDetails);
-
   } catch (error) {
     console.error( error);
+  }
+}
+
+export const removeFromFavorites = async (userId, itemId) => {
+  if (!user) {
+    throw new Error("User is not authenticated");
+  }
+  try {
+    await set(ref(database, `users/${userId}/favorites/${itemId}`), null);
+    console.log('teacher removed from favorites');
+  }
+  catch(e) {
+    e.message
   }
 }
